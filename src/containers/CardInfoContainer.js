8fa8e6feee878f3b4d9374ef8f4892c6a0d81bcc
jETA <jeta@jetalog.net>
2017-02-25 09:08:45 +0900

    Added margin-top 50px to all of containers.

--- src/containers/CardInfoContainer.js
+++ src/containers/CardInfoContainer.js
@@ -13,8 +13,12 @@ class CardInfoContainer extends React.Component {
     }
 
     render() {
+        let style = {
+          "marginTop": "50px"
+        };
+
         return(
-          <div>
+          <div style={style}>
             <CardInfo />
           </div>
         );
