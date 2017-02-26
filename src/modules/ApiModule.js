06644abba78ddb7db5f2fd164edb7a6446c03383
jETA <jeta@jetalog.net>
2017-02-26 11:55:42 +0900

    Changed account for show.

--- src/modules/ApiModule.js
+++ src/modules/ApiModule.js
@@ -56,7 +56,7 @@ export function getUserList(token, callback) {
 }
 
 export function getBadgeHistory(year, month, callback) {
-    const path = "/api/user/status/-temp-kakao-369757425/" + year + "/" + (month + 1);
+    const path = "/api/user/status/-temp-kakao-375867508/" + year + "/" + (month + 1);
 
     console.log(path);
 
