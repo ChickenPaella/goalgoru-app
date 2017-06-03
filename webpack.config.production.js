ce35c3501aa03f531a22b7ea58e11c762d7b84f8
ta3ke <admin@ta3ke.com>
2017-06-03 20:01:00 +0900

    add weppack pakage command
    Signed-off-by: ta3ke <admin@ta3ke.com>

---
+++ webpack.config.production.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 3000,
+        historyApiFallback: {
+            rewrites: [{ from: /^(?!\/assets\/)/i, to: '/index.html' }]
+        },
+        contentBase: __dirname + '/public'
+    },
+
+    module:
+    {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'stage-0', 'react']
+                }
+            }
+        ]
+    }
+};
